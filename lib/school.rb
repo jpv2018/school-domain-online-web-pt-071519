731fb528b303db68215b9db8fdfc3ea53f0baadd
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-18 19:50:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    if @roster[grade]
+      @roster{grade=>[name]}
+    else 
+      @roster[grade] = {}
+      @roster[grade]=>[name]
+    end
+    
+  end
+  def grade=(grade)
+    @grade = roster[grade]
+  end
+  
+  def grade(grade)
+    @grade = roster[grade]
+  end
+  
+  def sort=(roster)
+    @sort = roster.sort
+  end
+  
+  def sort
+    @sort = roster.sort
+  end
+end
